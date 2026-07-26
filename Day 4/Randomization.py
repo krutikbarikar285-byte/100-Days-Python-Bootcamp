76952582ef85decbdc64922672e8da22e9dba9c8
krutikbarikar285-byte <krutikbarikar285@gmail.com>
2026-07-26 16:19:05 +0530

    Refactor comments and maintain code structure
    Corrected comments and formatting in the Randomization.py file.

--- Day 4/Randomization.py
+++ Day 4/Randomization.py
@@ -3,13 +3,16 @@ import random
 random_number=random.randint(0,1)
 # randint called using the module name
 print(random_number)
+
+
 #below randomissation of floating number is used
 random_numer_1_to_2=random.random()
-#Here the random function andoms the number between 0.0<=N<1.0
-# and below is another function called uniform that randoms brtween two numbers includingg both the number 
+#Here the random function randoms the number between 0.0<=N<1.0
+# and below is another function called uniform that randoms between two numbers includingg both the number 
 random_floating_number=random.uniform(1,10)
 print(random_numer_1_to_2)
 print(random_floating_number)
+
 
 if random_number == 0 :
     print("Heads")
