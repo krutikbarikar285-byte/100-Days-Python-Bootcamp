77044f73f5547c0f6f0a99c641bc17e313faeb06
krutikbarikar285-byte <krutikbarikar285@gmail.com>
2026-07-30 22:42:40 +0530

    Add files via upload

---
+++ Day 5/Creating our own built-in functions.py
@@ -0,0 +1,9 @@
+list=[21,453,786,345,678,543,786]
+#using builtin func to sum the list
+print(sum(list))
+
+#creating our own sum using for loop
+item=0
+for i in  list :
+    item+=i
+print(item)    
